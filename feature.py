4d779c203f37025530337e8ae120221e075f01e6
牛尾凌太 <ryotaushio@ushioryoutanoMacBook-Pro.local>
2021-01-25 05:18:15 +0900

    added auto_encoder()

--- feature.py
+++ feature.py
@@ -1,6 +1,29 @@
 """Feature Extraction Processors."""
 
 import numpy as np
+import nn
+
+
+def project(X, basis):
+    r"""与えられたデータ集合を、与えられた基底に射影して変換する。
+
+    Parameters
+    ----------
+    X : array_like
+        データを行として並べた行列
+    basis : array_like
+        基底を行として並べた行列
+
+    Returns
+    -------
+    np.ndarray
+        射影後のデータを行として並べた行列
+    """
+    X_projected = np.matmul(X, basis.T)
+    if np.all(np.imag(X_projected) == 0):
+        return X_projected.astype(float)
+    return X_projected
+
 
 
 def pca(X):
@@ -33,22 +56,19 @@ def pca(X):
     return eigvecs, ctrb
 
 
-def project(X, basis):
-    r"""与えられたデータ集合を、与えられた基底に射影して変換する。
+def auto_encoder(X, n_hidden_units, *args, **kwargs):
+    n_sample, n_feature = X.shape
+    net = nn.mlp.from_shape(
+        shape=[n_feature, n_hidden_units, n_feature],
+        act_funcs=[None, 'linear', 'linear']
+    )
+    log = net.train(X=X, T=X, *args, **kwargs)
+    return net, log
 
-    Parameters
-    ----------
-    X : array_like
-        データを行として並べた行列
-    basis : array_like
-        基底を行として並べた行列
+# net, log = ft.auto_encoder(
+#     mnist.X_train, 32, eta=0.1,
+#     optimizer='AdaGrad', how_to_show='plot',
+#     batch_size=200, log_cond=lambda i: i%1000==0
+# )
+# mnist.imshow(net[1].W[:, 2].reshape(28,28)[3:-3, 3:-3])
 
-    Returns
-    -------
-    np.ndarray
-        射影後のデータを行として並べた行列
-    """
-    X_projected = np.matmul(X, basis.T)
-    if np.all(np.imag(X_projected) == 0):
-        return X_projected.astype(float)
-    return X_projected
