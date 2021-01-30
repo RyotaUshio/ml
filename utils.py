da696d2811825d6c92f247312b8a3bed0e016303
牛尾凌太 <ryotaushio@ushioryoutanoMacBook-Pro.local>
2021-01-30 21:54:55 +0900

    added labels_dict for fashion_mnist

--- utils.py
+++ utils.py
@@ -12,6 +12,19 @@ DATASETS = {
     'cifar100'      : datasets.cifar100
 }
 
+fashion_mnist_labels = {
+    0 : 'T-shirt/top',
+    1 : 'Trouser',
+    2 :	'Pullover',
+    3 :	'Dress',
+    4 :	'Coat',
+    5 :	'Sandal',
+    6 :	'Shirt',
+    7 :	'Sneaker',
+    8 :	'Bag',
+    9 :	'Ankle boot'
+}
+
 ### データを読み込む
 def load(
         data='mnist',
