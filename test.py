c823b4b5f179c7919d10b3851c74630aa787f629
牛尾凌太 <ryotaushio@ushioryoutanoMacBook-Pro.local>
2021-01-31 13:12:12 +0900

    invereted dropout test

--- test.py
+++ test.py
@@ -3,12 +3,15 @@ import numpy as np
 
 (X_train, T_train), (X_test, T_test) = utils.load_data('mnist')
 
-net = nn.mlp_classifier.from_shape([784, 500, 200, 10], dropout_ratio=[0.1, 0.4, 0.4])
-net.train(X_train=X_train, T_train=T_train,
-          X_val=X_test, T_val=T_test,
-          eta0=1e-2, lamb=0.0001, optimizer='Momentum',
-          max_epoch=830, 
-          early_stopping=False)
+net = nn.mlp_classifier.from_shape(
+    [784, 500, 200, 10], dropout_ratio=[0.1, 0.4, 0.4], inverted=True
+)
+net.train(
+    X_train=X_train, T_train=T_train,
+    X_val=X_test, T_val=T_test,
+    eta0=1e-2, lamb=0.0001, optimizer='Momentum',
+    max_epoch=830, early_stopping=False
+)
 
 print("Train: ", end="")
 net.test(X_train, T_train, log=False, verbose=True)
