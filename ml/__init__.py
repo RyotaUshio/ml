6b056390b7245a619fd0a38d505e8fbb4e19c1ed
Ryota Ushio <gt0410ushi@g.ecc.u-tokyo.ac.jp>
2021-02-06 14:56:09 +0900

    added doc in __init__.py

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,5 +1,11 @@
+"""ml - Pattern Recognition & Machine Learning Package for Python
+==============================================================
+"""
+
 from . import base, exceptions, nn, utils, classify, cluster, feature
 from .evaluate import evaluate
 from .utils import load, save, load_data, imshow, scatter
 
-__all__ = ['base', 'exceptions', 'utils', 'nn', 'classify', 'cluster', 'feature', 'evaluate']
+__version__ = '0.0.1'
+
+__all__ = ['base', 'exceptions', 'utils', 'nn', 'classify', 'cluster', 'feature', 'evaluate', 'load', 'save', 'load_data', 'imshow', 'scatter']
--- ml/utils.py
+++ ml/utils.py
@@ -13,17 +13,19 @@ DATASETS = {
     'cifar100'      : datasets.cifar100
 }
 
-fashion_mnist_labels = {
-    0 : 'T-shirt/top',
-    1 : 'Trouser',
-    2 :	'Pullover',
-    3 :	'Dress',
-    4 :	'Coat',
-    5 :	'Sandal',
-    6 :	'Shirt',
-    7 :	'Sneaker',
-    8 :	'Bag',
-    9 :	'Ankle boot'
+LABELS = {
+    'fashion_mnist' : {
+        0 : 'T-shirt/top',
+        1 : 'Trouser',
+        2 : 'Pullover',
+        3 : 'Dress',
+        4 : 'Coat',
+        5 : 'Sandal',
+        6 : 'Shirt',
+        7 : 'Sneaker',
+        8 : 'Bag',
+        9 : 'Ankle boot'
+    }
 }
 
 ### データを読み込む
