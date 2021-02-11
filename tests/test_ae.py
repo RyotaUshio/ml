b26a981b2da02d8813f5ede70d64b9aee668545f
Ryota Ushio <gt0410ushi@g.ecc.u-tokyo.ac.jp>
2021-02-11 23:57:51 +0900

    added 'patience_epoch=30'

--- tests/test_ae.py
+++ tests/test_ae.py
@@ -9,10 +9,15 @@ ae = ml.feature.autoencoder.fit(
     X_train, [128, 64, 32], 
     encode_act='ReLU', decode_act=['sigmoid', 'ReLU', 'ReLU'], 
     X_val=X_test,
-    lamb=0.0001,eta0=0.0001, 
-    optimizer='Momentum', max_epoch=500
+    eta0=0.0001,
+    optimizer='Momentum',
+    lamb=0.0001,
+    max_epoch=500,
+    patience_epoch=30
 )
 
-# ae.save('pkl/ae')
+ae.save('pkl/ae')
 ml.imshow(ae(X_train[0]).flatten())
+plt.pause(1)
 ml.imshow(ae(X_test[0]).flatten())
+plt.pause(1)
